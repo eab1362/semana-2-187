abe2be11d1abe5ab64f24d78270b86c9e39a9222
Edgar Andrade <eab1362@hotmail.com>
2020-12-05 13:23:40 -0500

    primer commit

---
+++ src/components/Noticias.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Noticias',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
