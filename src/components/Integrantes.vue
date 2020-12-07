042f00a92159108a2a0149b42d3c222151af6786
Edgar Andrade <eab1362@hotmail.com>
2020-12-07 00:01:23 -0500

    bug

--- src/components/Integrantes.vue
+++ src/components/Integrantes.vue
@@ -1,70 +1,74 @@
 <template>
-        <div class="container">
-             <div class="row">
-            <div class="integrantes-container" v-for="(Itemtem, index) in items" :key="index" >
-<div class="col-sm">
-                <teamCard
-                    v-bind:image="Itemtem.image"
-                    v-bind:nombre="Itemtem.nombre"
-                    v-bind:descripcion="Itemtem.descripcion"
-                    v-bind:rol="Itemtem.rol"
-                />
-</div>
-            </div>
-             </div>
+  <div class="container  ">
+    <div class="d-inline-flex ">
+      <div
+        class="integrantes-container"
+        v-for="(Itemtem, index) in items"
+        :key="index"
+      >
+        <div class=" d-inline-flex ">
+          <teamCard
+            v-bind:image="Itemtem.image"
+            v-bind:nombre="Itemtem.nombre"
+            v-bind:descripcion="Itemtem.descripcion"
+            v-bind:rol="Itemtem.rol"
+          />
         </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import teamCard from '@/components/TeamCard'
+import teamCard from "@/components/TeamCard";
 
 export default {
-    name: 'Integrantes',
-    components:{
-        teamCard,
-    },
+  name: "Integrantes",
+  components: {
+    teamCard,
+  },
 
-    data(){
-        return{
-        items:[
+  data() {
+    return {
+      items: [
         {
-            codigo: 1,
-            nombre: 'Edgar Andrade',
-            descripcion: '',
-            rol: 'Desarrollador Backend',
-            image: 'https://placeimg.com/200/300/people'
+          codigo: 1,
+          nombre: "Edgar Andrade",
+          descripcion: "",
+          rol: "Desarrollador Backend",
+          image: "https://placeimg.com/200/300/people",
         },
         {
-        codigo: 2,
-        nombre: 'Fredy Cardona',
-        descripcion: '',
-        rol: 'Desarrollador Backend',
-        image: 'https://placeimg.com/200/300/people'
-      },
+          codigo: 2,
+          nombre: "Fredy Cardona",
+          descripcion: "",
+          rol: "Desarrollador Backend",
+          image: "https://placeimg.com/200/300/people",
+        },
 
-      {
-        codigo: 3,
-        nombre: 'Deciret Díaz',
-        descripcion: '',
-        rol: 'Desarrollador Frontend',
-        image: 'https://placeimg.com/200/300/people'
-      },
-            {
-        codigo: 4,
-        nombre: 'Hector Gomez',
-        descripcion: '',
-        rol: 'Desarrollador Frontend',
-        image: 'https://placeimg.com/200/300/people'
-      },
-      {
-        codigo: 5,
-        nombre: 'Héctor Melgarejo',
-        descripcion: '',
-        rol: 'Desarrollador Frontend',
-        image: 'https://placeimg.com/200/300/people'
-      }
-        ]
-        /*return{
+        {
+          codigo: 3,
+          nombre: "Deciret Díaz",
+          descripcion: "",
+          rol: "Desarrollador Frontend",
+          image: "https://placeimg.com/200/300/people",
+        },
+        {
+          codigo: 4,
+          nombre: "Hector Gomez",
+          descripcion: "",
+          rol: "Desarrollador Frontend",
+          image: "https://placeimg.com/200/300/people",
+        },
+        {
+          codigo: 5,
+          nombre: "Héctor Melgarejo",
+          descripcion: "",
+          rol: "Desarrollador Frontend",
+          image: "https://placeimg.com/200/300/people",
+        },
+      ],
+      /*return{
             codigo: 1,
             nombre: 'Edgar Andrade',
             descripcion: '',
@@ -72,6 +76,6 @@ export default {
             image: 'https://placeimg.com/200/300/people'
         };*/
     };
-    },
+  },
 };
 </script>
