057e02359baf2e4519f0cfa8a44916bb7a50e7ae
03DEIMOS <ladelrizo@gmail.com>
2020-12-06 23:45:32 -0500

    Commit Integrantes 4

--- src/components/Integrantes.vue
+++ src/components/Integrantes.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div id="integrantes" class="col-sm-6" style="background-color: lavender height: 800px" >
-        
-        <div v-for="(inte, index) of object"
-        :key="index" class="miembros" style="width: 400px height:600px display: grid; grid-template-columns: auto auto auto auto auto; background-color: azure;" >
-          <img class="miembro__item" {{ inte.image }} alt="Imagen del Integrante" style="margin: 6px; text-align: justify;  width: 200px; height: 300px;" />
-            <h2 style="text-align: center;">{{inte.nombre}}</h2>
-            <p class="card-text">{{inte.description}}</p>
-            <p class="card-text">{{inte.rol}}</p>
+        <div class="container">
+            <div class="integrantes-container" v-for="(Itemtem, index) in Items" :key="index" >
+
+                <teamCard
+                    v-bind:image="image"
+                    v-bind:nombre="nombre"
+                    v-bind:descripcion="descripcion"
+                    v-bind:rol="rol"
+                />
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
+import teamCard from '@/components/TeamCard'
 
-new Vue ({
-  el: '#integrantes',
-  data:{
-    object: [
-      {
-        codigo: 1,
-        nombre: 'Edgar Andrade',
-        descripcion: '',
-        rol: 'Desarrollador Backend',
-        image: 'https://placeimg.com/200/300/people'
-      },
+export default {
+    name: 'Integrantes',
+    components:{
+        teamCard,
+    },
 
-      {
+    data:{
+        items:[
+        {
+            codigo: 1,
+            nombre: 'Edgar Andrade',
+            descripcion: '',
+            rol: 'Desarrollador Backend',
+            image: 'https://placeimg.com/200/300/people'
+        },
+        {
         codigo: 2,
         nombre: 'Fredy Cardona',
         descripcion: '',
@@ -58,11 +59,14 @@ new Vue ({
         rol: 'Desarrollador Frontend',
         image: 'https://placeimg.com/200/300/people'
       }
-    ]
-  }
-});
+        ]
+        /*return{
+            codigo: 1,
+            nombre: 'Edgar Andrade',
+            descripcion: '',
+            rol: 'Desarrollador Backend',
+            image: 'https://placeimg.com/200/300/people'
+        };*/
+    },
+};
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="integrantes">
+    <div class="integrantes-row">
+      <img :src="image"/>
+      <h1 class ="integrantes-nombre">{{nombre}}</h1>
+      <div class="integrantes-descripcion">{{descripcion}}</div>
+      <div class="integrantes-rol">{{rol}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default{
+  name: 'TeamCard',
+
+  props:{
+    codigo:{
+      type: String,
+      require: true,
+    },
+    nombre:{
+      type: String,
+      require: true,
+    },
+    descripcion:{
+      type: String,
+      require: true,
+    },
+    rol:{
+      type: String,
+      require: true,
+    },
+    image:{
+      type: String,
+      require: true,
+    },
+  },
+};
+
+/*
+new Vue ({
+  el: '#integrantes',
+  data:{
+    object: [
+      {
+        codigo: 1,
+        nombre: 'Edgar Andrade',
+        descripcion: '',
+        rol: 'Desarrollador Backend',
+        image: 'https://placeimg.com/200/300/people'
+      },
+
+      {
+        codigo: 2,
+        nombre: 'Fredy Cardona',
+        descripcion: '',
+        rol: 'Desarrollador Backend',
+        image: 'https://placeimg.com/200/300/people'
+      },
+
+      {
+        codigo: 3,
+        nombre: 'Deciret Díaz',
+        descripcion: '',
+        rol: 'Desarrollador Frontend',
+        image: 'https://placeimg.com/200/300/people'
+      },
+            {
+        codigo: 4,
+        nombre: 'Hector Gomez',
+        descripcion: '',
+        rol: 'Desarrollador Frontend',
+        image: 'https://placeimg.com/200/300/people'
+      },
+      {
+        codigo: 5,
+        nombre: 'Héctor Melgarejo',
+        descripcion: '',
+        rol: 'Desarrollador Frontend',
+        image: 'https://placeimg.com/200/300/people'
+      }
+    ]
+  }
+});*/
+</script>
+
+<style scoped>
+
+</style>
